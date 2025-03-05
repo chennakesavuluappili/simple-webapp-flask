2a8909d220768ffce51e12b66bc7e13b809baa60
chennakesavuluappili <65659461+chennakesavuluappili@users.noreply.github.com>
2025-03-05 23:02:26 +0530

    Update 2 app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you my dear brother? say hi to everyone!!'
+    return 'I am good, how about you my dear brother? say hi to everyone!! chenna'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
